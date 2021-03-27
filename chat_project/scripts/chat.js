88d36c66050a7a50e7b6c2583a203044775cc5a0
Almedin Alibasic <alibasicalmedin@gmail.com>
2021-03-27 21:15:12 +0100

    finished

--- chat_project/scripts/chat.js
+++ chat_project/scripts/chat.js
@@ -3,6 +3,7 @@ class Chatroom {
         this.room = room;
         this.username = username;
         this.chats = db.collection('chats');
+        this.unsub;
     }
     async addChat (message) {
        const now = new Date();
@@ -15,9 +16,28 @@ class Chatroom {
        const response = await this.chats.add(chat);
        return response;
     }
+    getChats(callback){
+        this.unsub = this.chats
+        .where('room', '==', this.room)
+        .orderBy('created_at')
+        .onSnapshot(snapshot => {
+            snapshot.docChanges().forEach(change => {
+                if(change.type === 'added'){
+                    callback(change.doc.data());
+
+                }
+            });
+        });
+    }
+    updateName(username){
+        this.username = username;
+        localStorage.setItem('username', username);
+    }
+    updateRoom(room){
+        this.room = room;
+        console.log('room updated');
+        if(this.unsub){
+            this.unsub();
+        }
+    }
 }
-
-const chatroom = new Chatroom ('gaming', 'adara');
-
-chatroom.addChat('hello everyone').then(() => console.log('chat added'))
-.catch(err => console.log(err));
