73a7238c595cb3200d2a9dcb9e2c50db45ed9e6d
Almedin Alibasic <alibasicalmedin@gmail.com>
2021-03-29 01:29:13 +0200

    fixed bug

--- chat_project/scripts/ui.js
+++ chat_project/scripts/ui.js
@@ -12,8 +12,8 @@ class ChatUI {
         );
         const html = `
         <li class="list-group-item>
-        <span class="username>${data.username}</span>
-        <span class="message${data.message}</span>
+        <span class="username">${data.username}</span>
+        <span class="message">${data.message}</span>
           <div class="time">${when}</div>
         </li>
         `;
